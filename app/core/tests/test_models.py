23404dd88d33863520d5c4a9f31b7d3480de674e
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-06-05 11:53:22 +0400

    Implemented user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = "testemail@gmail.com"
+        password = "testpassword123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
